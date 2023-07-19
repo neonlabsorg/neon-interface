b7050977563a5d5fe27f6e1852fcf311fa92f795
Yuri Albuquerque <yuridenommus@gmail.com>
2023-07-19 12:28:08 -0300

    Config is no longer Clone

--- src/types.rs
+++ src/types.rs
@@ -6,7 +6,7 @@ use async_ffi::BorrowingFfiFuture;
 
 #[repr(C)]
 #[derive(StableAbi)]
-#[sabi(impl_InterfaceType(Sync, Send, Debug, Clone))]
+#[sabi(impl_InterfaceType(Sync, Send, Debug))]
 pub struct ConfigOpaque;
 
 pub type BoxedConfig<'borr> = DynTrait<'borr, RBox<()>, ConfigOpaque>;
