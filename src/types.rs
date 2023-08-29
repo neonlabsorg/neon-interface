4dbcd53d68f495f82c99801cc848e40a8a70b87f
Yuri Albuquerque <yuridenommus@gmail.com>
2023-08-29 10:38:05 -0300

    New neon-lib will use local futures

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,8 @@
 use abi_stable::std_types::{RResult, RString};
-use async_ffi::BorrowingFfiFuture;
+use async_ffi::LocalBorrowingFfiFuture;
 use serde::{Deserialize, Serialize};
 
-pub type RNeonResult<'a> = BorrowingFfiFuture<'a, RResult<RString, RString>>;
+pub type RNeonResult<'a> = LocalBorrowingFfiFuture<'a, RResult<RString, RString>>;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct NeonLibError {
